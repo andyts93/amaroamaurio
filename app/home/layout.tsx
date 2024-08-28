5d126f73700133759aff81cecd23d24cd06bf9f3
Andrea <app@shock-wave.it>
2024-08-28 10:59:29 +0200

    home

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+import { LuMail, LuInstagram } from "react-icons/lu";
+import { TbBrandTiktok } from "react-icons/tb";
 
 export const metadata: Metadata = {
   title: "Test",
@@ -15,12 +18,12 @@ export default function RootLayout({
         <div className="bg-[#fecb04] min-h-screen px-4 md:px-32 pt-6">
             <header className="mb-12">
                 <nav className="bg-[#fff97b] px-12 py-8 rounded-full border border-black shadow-brutal-md flex justify-between items-center">
-                    <h1 className="text-2xl font-bold text-black">Nome</h1>
-                    <ul className="hidden md:flex gap-6 font-light tracking-wider">
-                        <li>Home</li>
-                        <li>La storia</li>
-                        <li>La ricetta</li>
-                        <li>Acquista</li>
+                    <h1 className="text-4xl font-bold text-[#0a162a] uppercase">Amaurio</h1>
+                    <ul className="hidden md:flex gap-6 font-light tracking-wider text-[#0a162a]">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#storia">La storia</a></li>
+                        <li><a href="#ricetta">La ricetta</a></li>
+                        <li><a href="#acquista">Acquista</a></li>
                     </ul>
                     <button type="button" className="md:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -32,8 +35,22 @@ export default function RootLayout({
             <main className="pb-12">{children}</main>
         </div>
         <footer className="border-t border-black bg-amber-100 px-4 md:px-32 py-4">
-            <div>
-                <p>2024 &copy; All rights reserved</p>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+                <div>
+                    <h2 className="text-4xl font-bold uppercase mb-4">Amaurio</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce molestie enim vitae sem facilisis aliquam.</p>
+                </div>
+                <div>
+                    <h3 className="text-xl font-bold uppercase mb-4 font-outfit">Contatti</h3>
+                    <p className="flex gap-2 items-center"><LuMail />hello@amaroamaurio.it</p>
+                </div>
+                <div>
+                    <h3 className="text-xl font-bold uppercase mb-4 font-outfit">Seguici</h3>
+                    <div className="flex gap-4 text-6xl">
+                        <Link href="#" className="hover:text-pink-500 transition-all duration-300 cursor-pointer"><LuInstagram /></Link>
+                        <Link href="#" className="hover:text-pink-500 transition-all duration-300 cursor-pointer"><TbBrandTiktok /></Link>
+                    </div>
+                </div>
             </div>
         </footer>
     </>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,29 +3,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-        <div className="bg-cyan-50 rounded-xl p-6 shadow-brutal mb-8 border border-black">
-            <h2 className="text-6xl my-32 text-center">BANNER</h2>
+        <div className="bg-cyan-50 rounded-xl overflow-hidden shadow-brutal border border-black">
+            <Image src={'/images/banner-amaurio.png'} width={1477} height={981} alt="Banner" />
         </div>
 
-        <div className="bg-orange-100 rounded-xl p-6 shadow-brutal border border-black mb-8">
+        <section className="bg-orange-100 rounded-xl p-6 shadow-brutal border border-black mt-8" id="storia">
             <h2 className="text-3xl font-bold tracking-wider mb-4">La storia</h2>
             <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce molestie enim vitae sem facilisis aliquam. Ut id consectetur justo, id luctus urna. Integer non velit eu turpis tristique tempor. Ut nisi quam, finibus at ipsum at, egestas gravida nibh. Aenean eget nulla sollicitudin, pharetra felis quis, vehicula mauris. Aenean interdum ut dolor id viverra. Nulla rutrum leo vel libero accumsan, tempus malesuada quam viverra. Integer dictum venenatis justo, at congue libero efficitur vel. In sed ipsum in magna euismod auctor. Sed vestibulum risus faucibus justo iaculis pellentesque.</p>
 
             <p className="mb-2">Phasellus a posuere purus. Aliquam vel faucibus lectus. Praesent rhoncus pulvinar tellus quis mollis. Cras efficitur neque quis enim iaculis, commodo ultricies lorem finibus. Sed posuere arcu ac orci sagittis semper. Phasellus vitae aliquam leo. Quisque ut lorem at magna ultricies viverra sit amet in tortor. Nam ex ante, lobortis ut velit quis, lobortis gravida magna. Nulla id sapien molestie, varius nulla vel, gravida sem. Morbi auctor, justo a faucibus placerat, turpis augue molestie arcu, eu scelerisque augue odio sed felis. Curabitur quam arcu, euismod ut lacus ut, commodo fermentum lacus. Donec pharetra dapibus nibh, ac aliquam magna facilisis consequat. Quisque semper consectetur sodales. Quisque dui dui, interdum vitae arcu vitae, auctor dapibus mauris. Quisque id accumsan felis.</p>
 
             <p className="mb-2">Proin et tempus ipsum. Etiam euismod vestibulum urna nec porttitor. Integer non lacus et mi placerat eleifend vitae at nulla. Vivamus in risus rutrum, imperdiet elit at, tempus nisl. Maecenas bibendum maximus velit, aliquam auctor elit blandit et. Nunc feugiat eros eu ipsum finibus placerat. Sed vel turpis in sapien porta placerat. Maecenas et augue eget nisi ornare vehicula.</p>
-        </div>
+        </section>
 
-        <div className="bg-sky-100 rounded-xl p-6 shadow-brutal border border-black mb-8">
+        <section className="bg-sky-100 rounded-xl p-6 shadow-brutal border border-black mt-8" id="ricetta">
             <h2 className="text-3xl font-bold tracking-wider mb-4">La ricetta</h2>
             <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce molestie enim vitae sem facilisis aliquam. Ut id consectetur justo, id luctus urna. Integer non velit eu turpis tristique tempor. Ut nisi quam, finibus at ipsum at, egestas gravida nibh. Aenean eget nulla sollicitudin, pharetra felis quis, vehicula mauris. Aenean interdum ut dolor id viverra. Nulla rutrum leo vel libero accumsan, tempus malesuada quam viverra. Integer dictum venenatis justo, at congue libero efficitur vel. In sed ipsum in magna euismod auctor. Sed vestibulum risus faucibus justo iaculis pellentesque.</p>
 
             <p className="mb-2">Phasellus a posuere purus. Aliquam vel faucibus lectus. Praesent rhoncus pulvinar tellus quis mollis. Cras efficitur neque quis enim iaculis, commodo ultricies lorem finibus. Sed posuere arcu ac orci sagittis semper. Phasellus vitae aliquam leo. Quisque ut lorem at magna ultricies viverra sit amet in tortor. Nam ex ante, lobortis ut velit quis, lobortis gravida magna. Nulla id sapien molestie, varius nulla vel, gravida sem. Morbi auctor, justo a faucibus placerat, turpis augue molestie arcu, eu scelerisque augue odio sed felis. Curabitur quam arcu, euismod ut lacus ut, commodo fermentum lacus. Donec pharetra dapibus nibh, ac aliquam magna facilisis consequat. Quisque semper consectetur sodales. Quisque dui dui, interdum vitae arcu vitae, auctor dapibus mauris. Quisque id accumsan felis.</p>
 
             <p className="mb-2">Proin et tempus ipsum. Etiam euismod vestibulum urna nec porttitor. Integer non lacus et mi placerat eleifend vitae at nulla. Vivamus in risus rutrum, imperdiet elit at, tempus nisl. Maecenas bibendum maximus velit, aliquam auctor elit blandit et. Nunc feugiat eros eu ipsum finibus placerat. Sed vel turpis in sapien porta placerat. Maecenas et augue eget nisi ornare vehicula.</p>
-        </div>
+        </section>
 
-        <div className="bg-violet-100 rounded-xl p-6 shadow-brutal border border-black">
+        <section className="bg-violet-100 rounded-xl p-6 shadow-brutal border border-black mt-8" id="acquista">
             <h2 className="text-3xl font-bold tracking-wider mb-4">Acquista</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
                 <div className="border border-black rounded-xl p-4 shadow-brutal bg-white">
@@ -47,7 +47,7 @@ export default function Home() {
                     <button type="button" className="bg-violet-500 text-white px-4 py-2 rounded-full font-semibold shadow-brutal shadow-black border border-black w-full">Compra ora</button>
                 </div>
             </div>
-        </div>
+        </section>
     </>
   );
 }
