ac45c9621513157ab1bcf8db6b6013612f668f51
Andrea <app@shock-wave.it>
2024-08-28 15:55:14 +0200

    ottimizzazioni

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
                         <li><a href="#ricetta" aria-label="La ricetta">La ricetta</a></li>
                         <li><a href="#acquista" aria-label="Acquista">Acquista</a></li>
                     </ul>
-                    <button type="button" className="md:hidden" onClick={() => setMobileMenuOpen(true)}>
+                    <button type="button" className="md:hidden" onClick={() => setMobileMenuOpen(true)} aria-label="Apri menu">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
                         </svg>
