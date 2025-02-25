f2da10877adcb829175a3cf86a78502110db7195
Andrea <app@shock-wave.it>
2025-02-25 14:34:59 +0100

    ga-id

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ import { Toaster } from "react-hot-toast";
 import { GoPeople } from "react-icons/go";
 import { GrAnnounce } from "react-icons/gr";
 import { Analytics } from "@vercel/analytics/react"
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const chicle = Chicle({ weight: '400', subsets: ['latin'], variable: '--font-chickle' });
 const jost = Jost({ subsets: ['latin'], variable: '--font-jost' });
@@ -250,6 +250,7 @@ export default function RootLayout({
       <SpeedInsights />
       <Analytics/>
       <GoogleAnalytics gaId="G-QT17F0BC6Y" />
+      <GoogleTagManager gtmId="AW-16894964531"/>
       </body>
       <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
     </html>
